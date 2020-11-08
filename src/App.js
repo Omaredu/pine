48a62ee05ee6bc30cf478a57c75887d0d5e95500
Sofia <doradodx@gmail.com>
2020-11-08 16:32:31 -0600

    panel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { GoogleMap } from '@react-google-maps/api';
 import React, { Component } from 'react';
-import { LoginPanel, LeftPanel, SignupPanel, Button } from './components';
+import { LoginPanel, LeftPanel, SignupPanel, Button, Addingpanel } from './components';
 import Map from "./components/Map";
 
 export default class App extends Component {
   state = {
     loginShown: false,
     signupShown: false,
-    isLoggedIn: false,
+    isLoggedIn: true,
     adding: false
   }
 
@@ -19,15 +19,16 @@ export default class App extends Component {
   _toggleSignupPanel = () => {
     this.setState({ signupShown: !this.state.signupShown, loginShown: false })
   }
-    /*Ahorita vengo*/ /* okok, yo tambien ahi regreso*/
   render () {
     return (
       <div>
         <div class="rectangulo">
+
           <div className="buttons-container" style={{ display: !this.state.isLoggedIn ? 'flex' : 'none' }}>
             <Button onClick={this._toggleLoginPanel}>Iniciar Sesión</Button>
             <Button onClick={this._toggleSignupPanel}>Registrarse</Button>
           </div>
+
           <div className="buttons-container" style={{ display: this.state.isLoggedIn ? 'flex' : 'none' }}>
             <Button onClick={() => {}}>Cerrar Sesión</Button>
           </div>
@@ -40,9 +41,13 @@ export default class App extends Component {
           <div className="left-panel-container" style={{ display: this.state.isLoggedIn ? 'flex' : 'none' }}>      
             <LeftPanel />
           </div>
-          <Map/>
+          <div className="adding-panel-container">            
+            <Addingpanel />
           </div>
+  
+          <Map/>
         </div>
+      </div>
     );
   }
 }
